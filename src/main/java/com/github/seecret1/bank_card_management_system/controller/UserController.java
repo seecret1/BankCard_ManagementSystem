6055a9354eed9997e33e2251d92a52aa98c1d537
seecret <naintonight@gmail.com>
2026-02-24 23:16:22 +0300

    added user controller

---
+++ src/main/java/com/github/seecret1/bank_card_management_system/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.github.seecret1.bank_card_management_system.controller;
+
+import com.github.seecret1.bank_card_management_system.dto.request.CreateUserRequest;
+import com.github.seecret1.bank_card_management_system.dto.request.UpdateUserRequest;
+import com.github.seecret1.bank_card_management_system.dto.response.UserResponse;
+import com.github.seecret1.bank_card_management_system.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> findAll() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(
+            @PathVariable String id
+    ) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<UserResponse> findByEmail(
+            @PathVariable String email
+    ) {
+        return ResponseEntity.ok(userService.findByUsername(email));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserResponse> findByUsername(
+            @PathVariable String username
+    ) {
+        return ResponseEntity.ok(userService.findByUsername(username));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserResponse> create(
+            @RequestBody CreateUserRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.create(request));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserResponse> update(
+            @PathVariable String id,
+            @RequestBody UpdateUserRequest request
+    ) {
+        return ResponseEntity.ok(userService.update(id, request));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<UserResponse> delete(
+            @PathVariable String id
+    ) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
